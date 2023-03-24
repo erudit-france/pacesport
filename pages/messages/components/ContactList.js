a5297767f74dd5443521b88690bcc9f1a5541462
Zakaria <rhayatizakaria@gmail.com>
2023-03-24 01:08:25 +0100

    [chatroom] wip

--- pages/messages/components/ContactList.js
+++ pages/messages/components/ContactList.js
@@ -3,86 +3,24 @@ import Link from "next/link"
 import moment from "moment/moment"
 import 'moment/locale/fr'
 
-export default function ContactList() {
-    const now = new moment()
-    const data =
-    [
-        // {
-        //     name: 'Auchan',
-        //     logo: 'https://logo-marque.com/wp-content/uploads/2021/02/Auchan-Logo.png',
-        //     message: 'Merci pour votre retour',
-        //     time: now,
-        //     unreadNumber: 2,
-        // },
-        // {
-        //     name: 'Decathlon',
-        //     logo: 'https://logos-marques.com/wp-content/uploads/2020/09/Decathlon-logo.png',
-        //     message: 'Merci pour votre retour',
-        //     time: now,
-        //     unreadNumber: 0,
-        // },
-        // {
-        //     name: 'Grand frais',
-        //     logo: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Grand_Frais_logo.png',
-        //     message: 'Merci pour votre retour',
-        //     time: now,
-        //     unreadNumber: 0,
-        // },
-        // {
-        //     name: 'Grand frais',
-        //     logo: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Grand_Frais_logo.png',
-        //     message: 'Merci pour votre retour',
-        //     time: now,
-        //     unreadNumber: 0,
-        // },
-        // {
-        //     name: 'Grand frais',
-        //     logo: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Grand_Frais_logo.png',
-        //     message: 'Merci pour votre retour',
-        //     time: now,
-        //     unreadNumber: 0,
-        // },
-        // {
-        //     name: 'Grand frais',
-        //     logo: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Grand_Frais_logo.png',
-        //     message: 'Merci pour votre retour',
-        //     time: now,
-        //     unreadNumber: 0,
-        // },
-        // {
-        //     name: 'Grand frais',
-        //     logo: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Grand_Frais_logo.png',
-        //     message: 'Merci pour votre retour',
-        //     time: now,
-        //     unreadNumber: 0,
-        // },
-        // {
-        //     name: 'Grand frais',
-        //     logo: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Grand_Frais_logo.png',
-        //     message: 'Merci pour votre retour',
-        //     time: now,
-        //     unreadNumber: 0,
-        // }
-    ]  
-    const getDelai = (time) => ( moment(time).fromNow() )
-
-    const messages = data.map((user, i) => {
+export default function ContactList({chatRooms}) {
+    const list = chatRooms.map((chatRoom, i) => {
         return (
-            <Link href={`/messages/${i}`} key={i}>
+            <Link href={`/messages/${chatRoom.id}`} key={i}>
                 <Flex className="tw-relative hover:tw-bg-slate-50  
                         hover:tw-text-gray-800 hover:tw-shadow-inner tw-rounded-lg pb-3" 
                         py={'xs'} mb={"sm"} align={'center'}>
-                    <Avatar className="tw-shadow-md tw-bg-contain" radius={'xl'} size={'lg'} src={user.logo} />
+                    <Avatar className="tw-shadow-md tw-bg-contain" radius={'xl'} size={'lg'} src={chatRoom.logo} />
                     <Flex direction={"column"} ml={'md'}>
-                        <Text className="tw-font-semibold" size={'sm'}>{user.name}</Text>
-                        <Text size={"xs"} weight={"light"}>{user.message}</Text>
+                        <Text className="tw-font-semibold" size={'sm'}>{chatRoom.nom} {chatRoom.prenom}</Text>
+                        <Text size={"xs"} weight={"light"}>--</Text>
                     </Flex>
                     <Flex className="tw-absolute tw-right-3" direction={'column'}>
-                        <Indicator hidden={user.unreadNumber == 0} inline label={user.unreadNumber} 
-                                size={18} radius={"xl"}  right={16} color={'red'}></Indicator>
+                        {/* <Indicator hidden={chatRoom.unreadNumber == 0} inline label={chatRoom.unreadNumber} 
+                                size={18} radius={"xl"}  right={16} color={'red'}></Indicator> */}
                     </Flex>
-                    <Text className="tw-text-gray-500 tw-font-semibold tw-bottom-1 tw-right-1 tw-absolute tw-text-[.7rem]">
-                            {getDelai(user.time)}</Text>
+                    {/* <Text className="tw-text-gray-500 tw-font-semibold tw-bottom-1 tw-right-1 tw-absolute tw-text-[.7rem]">
+                            {getDelai(chatRoom.time)}</Text> */}
                 </Flex>
             </Link>
         )
@@ -91,8 +29,8 @@ export default function ContactList() {
     return (
         <>
             {
-                data.length > 0 ?
-                    messages
+                list.length > 0 ?
+                    list
                     : <Text align="center">Aucun message</Text>
             }
         </>
--- pages/messages/index.js
+++ pages/messages/index.js
@@ -1,15 +1,90 @@
-import { ScrollArea } from "@mantine/core";
-import { useDocumentTitle } from "@mantine/hooks";
+import { Avatar, Flex, Indicator, Modal, Paper, ScrollArea, Text } from "@mantine/core";
+import { useDisclosure, useDocumentTitle } from "@mantine/hooks";
+import { getCookie } from "cookies-next";
 import Head from "next/head"
-import { useState } from "react";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { RxPerson } from "react-icons/rx";
 import ContactList from "./components/ContactList";
 import SearchUser from "./components/SearchUser";
 import Layout from "./layout"
+import { FiArrowRight } from 'react-icons/fi'
+import axios from "axios";
 
+const UsersCard = ({users}) => {
+    const items = users.length == 0 
+        ? <Text align='center'>Aucun utilisateur</Text>
+        : users.map((user) => (
+        <Flex key={String(user.id)} justify='space-between' my={'sm'}>
+            <Flex direction={"row"}>
+                <Avatar radius={'xl'} className="tw-shadow-md" />
+                <Text className='tw-my-auto tw-ml-2 tw-font-semibold tw-text-gray-700'>{user.nom} {user.prenom}</Text>
+            </Flex>
+            <Flex direction={'column'} justify='center' align={'center'}>
+                <Link href='' className="tw-bg-teal-600 tw-rounded-full tw-p-2">
+                    <FiArrowRight size={18} className=" tw-bg-teal-600 tw-rounded-full tw-text-white" />
+                </Link>
+            </Flex>
+        </Flex>
+    ));
+    return (
+        <>{items}</>
+    )
+}
+
+const ChatHeader = () => {
+    const [opened, { open, close }] = useDisclosure(false);
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+      async function fetchData(){
+        //   setLoading(true)
+          const res = await axios(
+              '/api/chat/users',
+              {headers: { 'JWTAuthorization': `Bearer ${getCookie('token')}`}}
+          );
+  
+          setUsers(JSON.parse(res.data.data))
+        //   setLoading(false)
+        }
+        fetchData()
+    }, []);
+
+    return (
+        <>
+            <div className="tw-mb-4">
+                <Link href={''} onClick={open}
+                    className='tw-text-sm tw-border-[1px] tw-px-3 tw-py-1.5 tw-rounded-2xl
+                            tw-border-gray-300 hover:tw-bg-gray-50'>
+                    Annuaire
+                </Link>
+            </div>
+            
+            <Modal opened={opened} onClose={close} title="Liste des utilisateurs" centered>
+                <UsersCard users={users}/>
+            </Modal>
+        </>
+    )
+}
 
 export default function Page(){
     const [title, setTitle] = useState('Pace\'Sport - Messages');
     useDocumentTitle(title);
+    
+    const [chatRooms, setChatRooms] = useState([]);
+    useEffect(() => {
+        async function fetchData(){
+          //   setLoading(true)
+            const res = await axios(
+                '/api/chat/rooms',
+                {headers: { 'JWTAuthorization': `Bearer ${getCookie('token')}`}}
+            );
+    
+            setChatRooms(JSON.parse(res.data.data))
+          //   setLoading(false)
+          }
+          fetchData()
+      }, []);
+
 
     return (
         <>
@@ -17,11 +92,14 @@ export default function Page(){
                 <title>{title}</title>
             </Head>
             <section className="tw-mx-3 tw-pt-4">
-                <SearchUser />
+                <Paper className="tw-py-4 tw-bg-white tw-p-2 tw-rounded-lg">
+                    <ChatHeader />
+                    <SearchUser />
+                </Paper>
                 <ScrollArea className="tw-bg-white tw-p-2 tw-py-5 tw-rounded-3xl tw-mt-2" 
                     offsetScrollbars
                     style={{ height: 'calc(100vh - 220px)' }}>
-                    <ContactList />
+                    <ContactList chatRooms={chatRooms} />
                 </ScrollArea>
             </section>
         </>
