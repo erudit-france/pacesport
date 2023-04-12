7e89058e2e92ffac27ff342342efb0385c85f17d
Zakaria <rhayatizakaria@gmail.com>
2023-04-12 11:59:03 +0200

    [association] communication message

---
+++ pages/communication/add/index.js
@@ -0,0 +1,77 @@
+import { Flex, Text, Textarea } from "@mantine/core"
+import Layout from "./layout"
+import { useForm } from "@mantine/form";
+
+const PriceRow = ({credits, price, oldPrice}) => {
+    return (
+        <Flex className="tw-py-2">
+            <Flex className="tw-w-1/2">
+                <Text className="tw-text-white tw-font-semibold tw-mx-auto tw-my-auto" align="center" weight={'bold'} size={'lg'}>{credits} Crédits</Text>
+            </Flex>
+            <Flex className="tw-w-1/2 tw-flex tw-flex-col" direction={'col'}>
+                <Text className="tw-text-white tw-font-semibold" align="center" size={'lg'}>{price} €</Text>
+                {oldPrice &&
+                    <Text className="tw-text-gray-600 tw-line-through tw-font-semibold tw-text-sm" align="center" size={'lg'}>{oldPrice} €</Text>
+                }
+            </Flex>
+        </Flex>
+    )
+}
+
+export default function Page() {
+    const form = useForm({
+        initialValues: {
+            message: '',
+        },
+        validate: {
+          message: (value) => (value != '' ? null : 'Veuillez saisir un message'),
+        },
+    });
+
+    const submitHandler = (values) => {
+        console.log('values', values)
+    }
+
+    return (
+        <>
+            <div className="tw-container tw-mx-auto tw-px-2">
+                <form onSubmit={form.onSubmit((values) => submitHandler(values))}>
+                    <Textarea
+                        size="md"
+                        placeholder="Contenu du message"
+                        label="Contenu du message publicitaire"
+                        withAsterisk
+                        {...form.getInputProps('message')}/>
+
+                    <Flex justify={'center'} mt={'md'}>
+                        <button type="submit" 
+                            className="tw-bg-gray-200 hover:tw-bg-gray-300/75
+                                    tw-rounded-3xl tw-shadow-sm tw-px-4 tw-py-3">
+                            <span className="tw-text-gray-800">Envoyer</span>
+                            <span className="tw-text-yellow-700"> 1 crédit</span>
+                        </button>
+                    </Flex>
+
+                    <Text className="tw-text-gray-700" mt="xl" mb='xs' size={'sm'} align="center">Un crédit Offert par mois</Text>
+                    <Text className="tw-text-gray-700" size={'sm'} align="center">Un crédit = une publication par jour</Text>
+                </form>
+            </div>
+
+            {/* gold pricing section */}
+            <section className="tw-bg-yellow-600/60 tw-mt-4">
+                <PriceRow credits={3} price={5.99} />
+                <PriceRow credits={10} price={12.99} oldPrice={19.99} />
+                <PriceRow credits={20} price={15.99} oldPrice={39.99} />
+                <PriceRow credits={40} price={19.99} oldPrice={79.99} />
+            </section>
+
+
+        </>
+    )
+}
+
+Page.getLayout = function getLayout(page) {
+    return (
+      <Layout>{page}</Layout>
+    )
+  }
---
+++ pages/communication/components/GreyHeader.js
@@ -0,0 +1,14 @@
+import { Text } from "@mantine/core";
+import Image from "next/image";
+import hero from '@/public/hand-in-hand.jpg'
+
+export default function GreyHeader({text}) {
+    return (
+        <header className='tw-flex tw-justify-center tw-h-32 tw-relative 
+            tw-bg-gradient-to-b tw-from-gray-50 tw-to-gray-200
+            tw-w-full tw-align-middle'>
+            <Text weight={'bold'} transform={'uppercase'} align="center"
+                className="tw-my-auto tw-text-gray-700 tw-pb-4">{text}</Text>
+        </header>
+    )
+}
