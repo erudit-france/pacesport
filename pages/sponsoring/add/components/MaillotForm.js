646575cc6abd057453c51373e509e04e550a7502
Zakaria <rhayatizakaria@gmail.com>
2023-04-13 15:12:01 +0200

    [sponsoring] offre sponsoring maillot & panneau

--- pages/sponsoring/add/components/MaillotForm.js
+++ pages/sponsoring/add/components/MaillotForm.js
@@ -1,11 +1,13 @@
 import { Carousel } from "@mantine/carousel";
-import { Button, Flex, Image, Modal, NumberInput, Text, TextInput, Textarea } from "@mantine/core";
+import { Button, Flex, Group, Image, Modal, NumberInput, Paper, Text, TextInput, Textarea } from "@mantine/core";
 import { useState } from "react";
 import { GoPlus } from 'react-icons/go'
 import { BsCurrencyEuro } from 'react-icons/bs'
+import MaillotModal from "./MaillotModal";
 
 export default function MaillotForm() {
     const [opened, setOpened] = useState(false);
+    const [maillotOffers, setMaillotOffers] = useState([]);
     const maillotImages = [
         {text: 'Gauche', src: null},
         {text: 'Droite', src: null},
@@ -13,44 +15,52 @@ export default function MaillotForm() {
         {text: 'Arrière', src: null}
     ]
 
+    const offers = maillotOffers.map((offer, idx) => (
+        <Paper key={idx} radius={'lg'} shadow="md" p={'lg'} mb={'md'}>
+            <Text align="center" color="dimmed" fz={'sm'}>{offer.img.text}</Text>
+            <Image src={offer.img.src} height={100} alt={offer.img.text} withPlaceholder/>
+            <Flex my={'lg'} direction={'column'}>
+                <Group my={'lg'} grow>
+                    <TextInput
+                        className="tw-font-semibold"
+                        variant="filled"
+                        readOnly
+                        description="Nom"
+                        value={offer.nom}
+                        />
+                    <TextInput
+                        className="tw-font-semibold"
+                        variant="filled"
+                        readOnly
+                        description="Description"
+                        value={offer.description}
+                        />
+                </Group>
+                <TextInput
+                    className="tw-font-semibold"
+                    variant="filled"
+                    readOnly
+                    description="Prix"
+                    value={offer.prix}
+                    rightSection={<BsCurrencyEuro />}
+                    />
+            </Flex>
+        </Paper>
+    ))
+
     return (
         <>
-            <Flex>
+            {offers}
+            <Flex mt={'md'}>
                 <Button variant="outline" radius={'lg'} rightIcon={<GoPlus />}
                     className="tw-border-gray-800 tw-text-gray-800 tw-mx-auto" 
                     onClick={() => setOpened(true)}>
                     Ajouter un emplacement</Button>
             </Flex>
-            <Modal
-                opened={opened}
-                onClose={() => setOpened(false)}
-                title="Ajouter emplacement maillot"
-            >
-                <Carousel my={'lg'} height={200} slideGap="sm" controlsOffset="md" withIndicators>
-                    {maillotImages.map((img) => (
-                        <Carousel.Slide key={img.text}>
-                            <Flex className="tw-flex-col tw-mx-auto">
-                                <Text fz={'sm'} dimmed={true} align="center">{img.text}</Text>
-                                <Image  
-                                    className="tw-mx-auto"       
-                                    height={200}
-                                    src={img.src}
-                                    alt="With default placeholder"
-                                    withPlaceholder/>
-                            </Flex>
-                        </Carousel.Slide>
-                    ))}
-                </Carousel>
-
-                <TextInput mb={'lg'} label="Nom de l'offre" withAsterisk/>
-                <Textarea mb={'lg'} autosize label="Description" />
-                <NumberInput mb={'xl'} min={0} rightSection={<BsCurrencyEuro />} />
-
-                <Flex>
-                    <Button className="tw-mx-auto tw-bg-lime-600 hover:tw-bg-lime-700" radius={'lg'}>Ajouter</Button>
-                </Flex>
-
-            </Modal>
+            <MaillotModal 
+                opened={opened} setOpened={setOpened} 
+                maillotOffers={maillotOffers} setMaillotOffers={setMaillotOffers}
+                maillotImages={maillotImages}/>
         </>
     )
 }
