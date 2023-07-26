5f7b6af96f2d7f56e96eb5c178e785cbcbde1b96
Zakaria <rhayatizakaria@gmail.com>
2023-07-26 16:39:03 +0200

    [offer type] badge update

--- components/SponsoringOfferTypeBadge.js
+++ components/SponsoringOfferTypeBadge.js
@@ -1,11 +1,18 @@
 import { Badge } from "@mantine/core"
 
 export default function SponsoringOfferTypeBadge({offer}) {
-    let type = 'Nationale'
-    let color = 'cyan'
-    if (offer.association) {
-        type = 'Locale'
-        color = 'green'
+    let type = 'Locale'
+    let color = 'green'
+    if (offer.type) {
+        // Nationale
+        if (offer.type == 'Nationale') {
+            type = 'Nationale'
+            color = 'cyan'
+        // Locale
+        } else {
+            type = 'Locale'
+            color = 'green'
+        }
     }
     return (
         <Badge size="xs" className="tw-font-normal tw-shadow-sm" variant="outline" color={color}>{type}</Badge>
