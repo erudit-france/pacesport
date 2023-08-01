88252bedd4de9cda8a2277aa0adc2680171dc050
Zakaria <rhayatizakaria@gmail.com>
2023-08-01 16:19:47 +0200

    [fix] avatar background placeholder fallback

--- components/AssociationCarte.js
+++ components/AssociationCarte.js
@@ -11,26 +11,59 @@ export default function AssociationCarte({organisation, href}) {
     const backgroundSrc = organisation.backgroundImage?.name
         ? `/uploads/${organisation.backgroundImage?.name}`
         : null
+
+    const BackgroundImage = () => {
+        if (backgroundSrc == null){
+            return (
+                <Image
+                className='tw-object-cover blur'
+                withPlaceholder
+                height={50}
+                alt={organisation.name}
+                />
+            )
+        }
+        return (
+            <Image
+            className='tw-object-cover blur'
+            src={`${backgroundSrc}`}
+            height={50}
+            alt={organisation.name}
+            />
+        )
+    }
+
+    
+    const AvatarImage = () => {
+        if (backgroundSrc == null){
+            return (
+                <Image
+                className='tw-object-cover blur'
+                withPlaceholder
+                height={50}
+                alt={organisation.name}
+                />
+            )
+        }
+        return (
+            <Avatar
+                className='tw-shadow-md tw-relative -tw-top-4 tw-opacity-100'
+                radius={'xl'}
+            />
+        )
+    }
+    
+
     
     return (
         <Link href={href}>
             <Card className='tw-bg-gradient-to-br tw-from-gray-50 tw-to-white tw-border-0 tw-relative tw-mx-3 tw-py-3 tw-shadow-md' shadow="md" radius="lg" withBorder>
                 <Card.Section className='tw-relative'>
-                    <div className='tw-w-full tw-h-full tw-px-4 tw-absolute tw-bg-slate-700/20 tw-z-20'>
-                    </div>
-                    <Image
-                    className='tw-object-cover blur'
-                    src={`${backgroundSrc}`}
-                    height={50}
-                    alt={organisation.name}
-                    />
+                    <div className='tw-w-full tw-h-full tw-px-4 tw-absolute tw-bg-slate-700/20 tw-z-20'></div>
+                    <BackgroundImage />
                 </Card.Section>
 
-                <Avatar
-                    className='tw-shadow-md tw-relative -tw-top-4 tw-opacity-100'
-                    radius={'xl'}
-                    src={`${avatarSrc}`}
-                 />
+                <AvatarImage />
 
                 <Box className='tw-relative -tw-top-2'>
                     <Text fz={'sm'} className='' weight={400}>{organisation.name}</Text>
