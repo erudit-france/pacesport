962ad72595c8a7b304fc8e3f3b49904e72c8ae9a
Zakaria <rhayatizakaria@gmail.com>
2023-08-02 13:42:50 +0200

    [card] image

--- pages/profil/association/components/CampagneCard.js
+++ pages/profil/association/components/CampagneCard.js
@@ -1,4 +1,4 @@
-import { AspectRatio, Badge, Box, Button, Flex, Image, Text } from "@mantine/core";
+import { AspectRatio, Badge, Box, Button, Center, Flex, Image, Text } from "@mantine/core";
 import moment from "moment";
 import Link from "next/link";
 
@@ -17,21 +17,23 @@ export default function CampagneCard({id, title, image, startDate, status}){
     return (
         <>
             <Flex className="tw-mb-7 tw-justify-around lg:tw-max-w-2xl lg:tw-mx-auto">
-                    <Box className="tw-rounded-xl tw-shadow-md tw-relative">
-                        <Image
-                            className="tw-absolute tw-right-1 tw-z-10 tw-opacity-80 -tw-translate-y-full tw-top-1/2"
-                            width={26}
-                            height={26}
-                            src={`/sim.png`}
-                            alt="logo sim"
-                        />
-                        <Image
-                            width={180}
-                            height={90}
-                            src={`/uploads/${image}`}
-                            alt="Photo de campagne"
-                        />
-                    </Box>
+                    <Center>
+                        <Box className="tw-rounded-xl tw-shadow-md tw-relative">
+                            <Image
+                                className="tw-absolute tw-right-1 tw-z-10 tw-opacity-80 -tw-translate-y-full tw-top-1/2"
+                                width={26}
+                                height={26}
+                                src={`/sim.png`}
+                                alt="logo sim"
+                            />
+                            <Image
+                                width={180}
+                                height={90}
+                                src={`/uploads/${image}`}
+                                alt="Photo de campagne"
+                            />
+                        </Box>
+                    </Center>
                     <Flex direction={'column'} ml={'lg'} justify={'space-around'}>
                         <Status />
                         <Text fz={'sm'} className="tw-capitalize">{nbCartes}&nbsp; Carte{nbCartes > 1 ? 's' : ''}</Text>
