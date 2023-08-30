e10c01f4a013f7e6ec18de950194677a55f96dad
TomTom <tom_deprez@hotmail.fr>
2023-08-30 16:00:36 +0200

    print error

--- components/admin/OffresTable.js
+++ components/admin/OffresTable.js
@@ -7,8 +7,8 @@ import { FaDownload } from "react-icons/fa";
 import { ImCross } from "react-icons/im";
 import SponsoringOfferTypeBadge from "../SponsoringOfferTypeBadge";
 
-export default function OffresTable({offres, refresh, fetching, editOffer}) {
-    const StatusBadge = ({offer}) => {
+export default function OffresTable({ offres, refresh, fetching, editOffer }) {
+    const StatusBadge = ({ offer }) => {
         let color = ''
         let text = ''
         if (offer.validated == 1 && offer.status == 1 || offer.association == null && offer.validated == 1) {
@@ -32,18 +32,21 @@ export default function OffresTable({offres, refresh, fetching, editOffer}) {
             method: 'POST',
             type: 'cors',
             headers: new Headers({
-              'JWTAuthorization': `Bearer ${getCookie('token')}`
+                'JWTAuthorization': `Bearer ${getCookie('token')}`
             }),
-            body: JSON.stringify({offer: id})
-          })
+            body: JSON.stringify({ offer: id })
+        })
             .then(res => res.json())
             .then(res => {
-                res.data.code == 1 
+                res.data.code == 1
                     ? Toast.success(res.data.message)
                     : Toast.error(res.data.message)
                 refresh()
             })
-            .catch((error) => { Toast.error('Erreur') })
+            .catch((error) => {
+                console.error("Erreur détaillée:", error);
+                Toast.error('Erreur')
+            })
     }
 
     const declineOffer = (id) => {
@@ -51,13 +54,13 @@ export default function OffresTable({offres, refresh, fetching, editOffer}) {
             method: 'POST',
             type: 'cors',
             headers: new Headers({
-              'JWTAuthorization': `Bearer ${getCookie('token')}`
+                'JWTAuthorization': `Bearer ${getCookie('token')}`
             }),
-            body: JSON.stringify({offer: id})
-          })
+            body: JSON.stringify({ offer: id })
+        })
             .then(res => res.json())
             .then(res => {
-                res.data.code == 1 
+                res.data.code == 1
                     ? Toast.success(res.data.message)
                     : Toast.error(res.data.message)
                 refresh()
@@ -67,73 +70,73 @@ export default function OffresTable({offres, refresh, fetching, editOffer}) {
 
     const ths = (
         <tr>
-          <th>Sponsor</th>
-          <th>Offre</th>
-          <th className="tw-capitalize">état</th>
-          <th>Contrat</th>
-          <th>Catégorie</th>
-          <th></th>
+            <th>Sponsor</th>
+            <th>Offre</th>
+            <th className="tw-capitalize">état</th>
+            <th>Contrat</th>
+            <th>Catégorie</th>
+            <th></th>
         </tr>
     )
-    
-    const rows = fetching 
+
+    const rows = fetching
         ? <tr><td colSpan={'100%'}><Loader mx={'auto'} my={'lg'} size={'sm'} color="gray"></Loader></td></tr>
         : offres.map((element) => (
-        <tr key={element.id}>
-            <td>
-                <Group>
-                    <Avatar className="tw-shadow-md" size={'sm'} radius={'xl'}  src={`/uploads/${element.enseigne.avatar?.name}`} />
-                    <Text fz={'sm'}>{element.enseigne.description}</Text>
-                </Group>
-            </td>
-            <td>
-                <Group>
-                {element.associations.length == 1
-                ?   <Avatar className='tw-shadow-md' radius={'xl'} src={`/uploads/${element.associations[0].avatar?.name}`} />
-                : element.associations.length > 1
-                    ? <Avatar.Group spacing="sm">
-                        <Avatar className='tw-shadow-md' size={'sm'} radius={'xl'} src={`/uploads/${element.associations[0].avatar?.name}`} />
-                        <Avatar className='tw-shadow-md' size={'sm'} radius={'xl'} src={`/uploads/${element.associations[1].avatar?.name}`} />
-                        {element.associations.length > 2 &&
-                        <Avatar radius="xl" size={'sm'}>+{element.associations.length - 2}</Avatar>
+            <tr key={element.id}>
+                <td>
+                    <Group>
+                        <Avatar className="tw-shadow-md" size={'sm'} radius={'xl'} src={`/uploads/${element.enseigne.avatar?.name}`} />
+                        <Text fz={'sm'}>{element.enseigne.description}</Text>
+                    </Group>
+                </td>
+                <td>
+                    <Group>
+                        {element.associations.length == 1
+                            ? <Avatar className='tw-shadow-md' radius={'xl'} src={`/uploads/${element.associations[0].avatar?.name}`} />
+                            : element.associations.length > 1
+                                ? <Avatar.Group spacing="sm">
+                                    <Avatar className='tw-shadow-md' size={'sm'} radius={'xl'} src={`/uploads/${element.associations[0].avatar?.name}`} />
+                                    <Avatar className='tw-shadow-md' size={'sm'} radius={'xl'} src={`/uploads/${element.associations[1].avatar?.name}`} />
+                                    {element.associations.length > 2 &&
+                                        <Avatar radius="xl" size={'sm'}>+{element.associations.length - 2}</Avatar>
+                                    }
+                                </Avatar.Group>
+                                : <Avatar className='tw-shadow-md' radius={'xl'} src={null} />
                         }
-                        </Avatar.Group>
-                    : <Avatar className='tw-shadow-md' radius={'xl'} src={null} />
-                }
-                    <SponsoringOfferTypeBadge offer={element} />
-                    <Text fz={'sm'} weight={600}>{element.title}</Text>
-                    <Text fz={'sm'}>{element.description}</Text>
-                </Group>
-            </td>
-            <td>
-                <StatusBadge offer={element} />
-            </td>
-            <td>
-                {element.contrat &&
-                <ActionIcon color="violet" component="a" href={`/uploads/${element.contrat.name}`}>
-                    <FaDownload />
-                </ActionIcon>}
-            </td>
-            <td>{element.category?.label}</td>
-            <td>
-                <Group>
-                    
-                    <ActionIcon variant="light" color="red"
-                                size={'lg'}
-                                onClick={() => declineOffer(element.id) }><ImCross /></ActionIcon>
-                    <ActionIcon variant="light"
-                        size={'lg'}
-                        color="teal"
-                        onClick={() => acceptOffer(element.id) }><BsCheckLg /></ActionIcon>
-                    <ActionIcon variant="light"
-                        size={'lg'}
-                        color="gray"
-                        onClick={() => editOffer(element) }
+                        <SponsoringOfferTypeBadge offer={element} />
+                        <Text fz={'sm'} weight={600}>{element.title}</Text>
+                        <Text fz={'sm'}>{element.description}</Text>
+                    </Group>
+                </td>
+                <td>
+                    <StatusBadge offer={element} />
+                </td>
+                <td>
+                    {element.contrat &&
+                        <ActionIcon color="violet" component="a" href={`/uploads/${element.contrat.name}`}>
+                            <FaDownload />
+                        </ActionIcon>}
+                </td>
+                <td>{element.category?.label}</td>
+                <td>
+                    <Group>
+
+                        <ActionIcon variant="light" color="red"
+                            size={'lg'}
+                            onClick={() => declineOffer(element.id)}><ImCross /></ActionIcon>
+                        <ActionIcon variant="light"
+                            size={'lg'}
+                            color="teal"
+                            onClick={() => acceptOffer(element.id)}><BsCheckLg /></ActionIcon>
+                        <ActionIcon variant="light"
+                            size={'lg'}
+                            color="gray"
+                            onClick={() => editOffer(element)}
                         ><BsFillGearFill /></ActionIcon>
-                </Group>
-            </td>
-        </tr>
-    ))
+                    </Group>
+                </td>
+            </tr>
+        ))
 
     return (
         <>
