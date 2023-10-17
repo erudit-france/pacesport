d90c1507a52f761807b60c7418d0a0bf29e9af23
TomTom <tom_deprez@hotmail.fr>
2023-10-18 01:05:14 +0200

    a

--- pages/admin/partenaire.js
+++ pages/admin/partenaire.js
@@ -2,6 +2,7 @@ import { ActionIcon, Box, Center, CopyButton, Flex, Group, Modal, ScrollArea, Sp
 import Head from "next/head";
 import Layout from "./layout";
 import { FiUsers } from "react-icons/fi";
+import { getAllEnseignes } from "@/domain/repository/AdminRepository";
 import { MdOutlineLocalOffer, MdOutlineStore } from "react-icons/md";
 import { getUser, getUsers } from "@/domain/repository/UserRepository";
 import UserRoleBadge from "@/components/UserRoleBadge";
@@ -13,47 +14,11 @@ import { getCookie } from 'cookies-next'
 import Toast from "@/services/Toast";
 
 export default function Page(props) {
-  const [open, setOpen] = useState(false)
-  const [openUser, setOpenUser] = useState(null)
-  const [users, setUsers] = useState(props.users);
+  const [partenaires, setPartenaires] = useState(Array.isArray(props.partenaires) ? props.partenaires : []);
 
-  const modalHandler = (user) => {
-    if (user == false) {
-      setOpenUser(null)
-      setOpen(false)
-      return
-    }
-    setOpenUser(user)
-    setOpen(true)
-  }
-  const deleteUser = (userId) => {
-    fetch(`/api/user/delete/${userId}`, {
-      method: 'DELETE',
-      headers: new Headers({
-        'JWTAuthorization': `Bearer ${getCookie('token')}`
-      })
-    })
-      .then(res => res.json())
-      .then(res => {
-        if (res.message) {
-          Toast.success('Utilisateur supprimé et e-mail envoyé avec succès');
-          setUsers(users.filter(user => user.id !== userId));
-        } else {
-          Toast.error('Erreur lors de la suppression de l\'utilisateur');
-        }
-        console.log('res', res);
-      })
-      .catch(error => {
-        Toast.error('Une erreur est survenue');
-        console.error('Error:', error);
-      });
-  };
-  const confirmDelete = (userId) => {
-    const confirmation = window.confirm("Êtes-vous sûr de vouloir supprimer cet utilisateur ?");
-    if (confirmation) {
-      deleteUser(userId);
-    }
-  };
+
+
+
   const ths = (
     <tr>
       <th>Nom</th>
@@ -64,13 +29,13 @@ export default function Page(props) {
     </tr>
   )
 
-  const rows = users.map((user) => (
-    <tr key={user.id}>
-      <td><Text transform="capitalize">{user.nom}&nbsp;{user.prenom}</Text></td>
+  const rows = partenaires.map((partenaire) => (
+    <tr key={partenaire.id}>
+      <td><Text transform="capitalize">{partenaire.name}</Text></td>
       <td>
         <Flex>
-          <Text>{user.email}</Text>
-          <CopyButton value={user.email} timeout={1500}>
+          <Text>{partenaire.email}</Text>
+          <CopyButton value={partenaire.email} timeout={1500}>
             {({ copied, copy }) => (
               <Tooltip label={copied ? 'Copié' : 'Copier'} withArrow position="right">
                 <ActionIcon color={copied ? 'teal' : 'gray'} onClick={copy}>
@@ -81,17 +46,12 @@ export default function Page(props) {
           </CopyButton>
         </Flex>
       </td>
-      <td><UserRoleBadge roles={user.roles} /></td>
+      <td><UserRoleBadge roles={partenaire.address} /></td>
       <td>
-        <Center>
-          <ActionIcon onClick={() => confirmDelete(user.id)}>
-            <FiTrash2 />
-          </ActionIcon>
-        </Center>
+
       </td>
       <td>
-        <Center><ActionIcon onClick={() => modalHandler(user)}>
-          <RxInfoCircled /></ActionIcon></Center>
+
       </td>
     </tr>
   ))
@@ -99,45 +59,14 @@ export default function Page(props) {
   return (
     <>
       <ScrollArea className="tw-max-w-[100%]">
-        <Tabs.Panel value="utilisateurs" p={"md"}>
-          <Title order={6} align="left">Utilisateurs</Title>
+        <Tabs.Panel value="Partenaires" p={"md"}>
+          <Title order={6} align="left">Partenaires</Title>
           <Table fontSize={'sm'} striped withColumnBorders>
             <thead>{ths}</thead>
             <tbody>{rows}</tbody>
           </Table>
         </Tabs.Panel>
       </ScrollArea>
-
-      <Modal
-        opened={open}
-        onClose={() => modalHandler(false)}
-        title={<Title order={6}>Informations</Title>}
-        centered
-        size={'60vw'}
-      >
-        <TextInput readOnly description="Nom" value={openUser?.nom} />
-        <Space h={'md'} />
-        <TextInput readOnly description="Prénom" value={openUser?.prenom} />
-        <Space h={'md'} />
-        <TextInput readOnly description="Téléphone" value={openUser?.telephone} />
-        <Space h={'md'} />
-        <TextInput readOnly description="Email" value={openUser?.email}
-          rightSection={
-            <CopyButton value={openUser?.email} timeout={1500}>
-              {({ copied, copy }) => (
-                <Tooltip label={copied ? 'Copié' : 'Copier'} withArrow position="right">
-                  <ActionIcon color={copied ? 'teal' : 'gray'} onClick={copy}>
-                    {copied ? <TbCheck size={14} /> : <TbCopy size={14} />}
-                  </ActionIcon>
-                </Tooltip>
-              )}
-            </CopyButton>} />
-        <Space h={'md'} />
-        <Stack align="flex-start" spacing="xs">
-          <Text fz={'xs'} color="dimmed">Role</Text>
-          <UserRoleBadge roles={openUser?.roles} />
-        </Stack>
-      </Modal>
     </>
   )
 }
@@ -154,39 +83,10 @@ export async function getServerSideProps(context) {
       }
     }
   }
-  let avatar = await fetch(`${process.env.API_URL}/api/association/avatar`, {
-    headers: new Headers({
-      'JWTAuthorization': `Bearer ${token}`,
-    })
-  }
-  )
-  avatar = await avatar.json();
-  if (avatar.code == 401) {
-    return {
-      redirect: {
-        permanent: false,
-        destination: "/login"
-      }
-    }
-  }
-
-  let backgroundImage = await fetch(`${process.env.API_URL}/api/association/background`, {
-    headers: new Headers({
-      'JWTAuthorization': `Bearer ${token}`,
-    })
-  }
-  )
-  backgroundImage = await backgroundImage.json();
-  let users = await getUsers(token)
-  console.log('users', users)
-
-
-  // // Pass data to the page via props
+  let partenaires = await getAllEnseignes(token)
   return {
     props: {
-      backgroundImage: backgroundImage.filename,
-      avatar: avatar.filename,
-      users: JSON.parse(users.data)
+      partenaires: partenaires,
     }
   }
 }
