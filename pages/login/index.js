e2a5b8e86f5cf2527c7cca61e3d65ae421469160
christopher aboulin <christopher@erudit-france.com>
2023-11-02 23:03:33 +0100

    hg

--- pages/login/index.js
+++ pages/login/index.js
@@ -48,6 +48,12 @@ export default function Page() {
 
     }, []);
     
+    let kk;
+    try 
+    {
+      kk = getCookie("token_v3")
+    }catch(e){}
+
     const overlayHandler = (isVisible) => setVisible(isVisible);
     const overlayClass = visible ? 'fade-enter-active' : 'fade-exit-active';
     const mainContentClass = visible ? 'hidden' : 'visible';
@@ -85,7 +91,7 @@ export default function Page() {
                                     <LoginForm loading={overlayHandler} />
                                 </Tabs.Panel>
                             </Tabs>
-                            <span className="tw-text-gray-100 tw-text-xs">{getCookie("token_v3")}</span>
+                            <span className="tw-text-gray-100 tw-text-xs">{kk}</span>
                         </main>
                     </div>
                 )}
